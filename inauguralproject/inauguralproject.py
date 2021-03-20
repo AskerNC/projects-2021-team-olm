be367f6aaad04f996ab0f857de320641e54cf241
Mads Janell <you@example.com>
2021-03-20 16:52:59 +0100

    Hand in

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,1 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+We will not be using modules for this projects.
